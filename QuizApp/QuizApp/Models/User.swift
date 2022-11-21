481c7725533d2937a694e3c8f4b90ccdb820fbf2
TomasVNoctrus <Tmas.css@gmail.com>
2022-11-21 21:17:18 +0200

    Update User.swift

---
+++ QuizApp/QuizApp/Models/User.swift
@@ -0,0 +1,48 @@
+//
+//  User.swift
+//  QuizApp
+//
+//  Created by Dmitrij Aneicik on 2022-11-17.
+//
+
+import Foundation
+
+class User {
+    let username: String
+    var completedQuizCount: Int
+    var totalAccuracy: Int
+    private var userPoints: Int
+    var correctAnswersCount: Int
+
+    init(username: String) {
+        self.username = username
+        completedQuizCount = 0
+        totalAccuracy = 0
+        userPoints = 0
+        correctAnswersCount = 0
+    }
+
+    func calculatePointsForCorrectAnswer(points: Int) {
+        // Ka daryti teisingai atsakius klausima
+        userPoints += points
+        correctAnswersCount += 1
+    }
+
+    func calculatePointsForWrongAnswer(points: Int) {
+        // Ka daryti neteisingai atsakius klausima
+        userPoints -= points
+    }
+
+    func totalPoints() -> Int {
+        // grazinti kiek useris turi is viso tasku
+        return userPoints
+    }
+
+    func updateCompletedQuizCount() {
+        // iskvieciama baigus zaidimui, reikia zinoti kiek zaidimu is vis useris yra atlikes ir perskaiciuoti jo atsakymu taikluma
+    }
+
+    func calculateTotalAccuracy() {
+        // perskaiciuoti useriu atsakymu taikluma
+    }
+}
