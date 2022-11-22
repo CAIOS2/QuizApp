2165d259dc2fa18708bc9abedfdb70f1dd4c82c0
Rolandas-R <driveris@gmail.com>
2022-11-22 20:38:12 +0200

    pradzia darbo

--- QuizApp/QuizApp/Scenes/Quiz/QuizesViewController.swift
+++ QuizApp/QuizApp/Scenes/Quiz/QuizesViewController.swift
@@ -14,6 +14,7 @@ class QuizesViewController: UIViewController {
     //MARK: - Override
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadNextQuestion(isInitialQuestion: true)
         
         // Do any additional setup after loading the view.
     }
@@ -26,16 +27,40 @@ class QuizesViewController: UIViewController {
     @IBOutlet private weak var yellowButton: UIButton!
     @IBOutlet private weak var pointsLabel: UILabel!
     @IBOutlet private weak var questionLabel: UILabel!
+    @IBOutlet weak var currentResultsLabel: UILabel!
     
-    
-    /*
      // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    @IBAction func blueButtonTapped(_ sender: Any) {
+
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func redButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+    }
+    
+    func loadNextQuestion(isInitialQuestion: Bool) {
+        var currentQuestion = quizManager.loadQuestion(isInitialQuestion: isInitialQuestion)
+        questionLabel.text = currentQuestion.questionText
+        blueButton.setTitle(currentQuestion.answers[0], for: .normal)
+        greenButton.setTitle(currentQuestion.answers[1], for: .normal)
+        yellowButton.setTitle(currentQuestion.answers[3], for: .normal)
+        redButton.setTitle(currentQuestion.answers[2], for: .normal)
+        
+    }
+    
+
+    
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     //override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destination.
      // Pass the selected object to the new view controller.
-     }
-     */
+     //}
     
 }
