14a7136f5c780dec67fc38acac549234395f72e6
Kiril Krutiajev <nonamekk@tutanota.com>
2022-11-23 20:18:18 +0200

    'showAlert added 'onTapAction'

--- QuizApp/QuizApp/Scenes/Helpers/UIAlertController+showAlert.swift
+++ QuizApp/QuizApp/Scenes/Helpers/UIAlertController+showAlert.swift
@@ -9,8 +9,8 @@ import Foundation
 import UIKit
 
 extension UIViewController {
-    func showAlert(title: String, _ message: String) {
-        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+    func showAlert(title: String, _ message: String, onTapAction: ((UIAlertAction) -> Void)? = nil) {
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: onTapAction)
         
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertController.addAction(alertAction)
