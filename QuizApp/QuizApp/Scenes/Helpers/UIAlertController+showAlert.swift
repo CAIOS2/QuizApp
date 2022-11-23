b8ed2ca5d0151a09a151b9d746c66475f97a3afd
TomasVNoctrus <Tmas.css@gmail.com>
2022-11-23 18:57:43 +0200

    Update UIAlertController+showAlert.swift

--- QuizApp/QuizApp/Scenes/Helpers/UIAlertController+showAlert.swift
+++ QuizApp/QuizApp/Scenes/Helpers/UIAlertController+showAlert.swift
@@ -5,4 +5,21 @@
 //  Created by Dmitrij Aneicik on 2022-11-17.
 //
 
-import Foundation
+import UIKit
+
+
+extension UIAlertController {
+    
+    static func showErrorAlert(error: String, message: String) -> UIAlertController {
+        
+        
+        let action = UIAlertAction(title: "Ok", style: .default)
+            
+        let alert =  UIAlertController(title: "message", message: "error", preferredStyle: .alert)
+            alert.addAction(action)
+            
+        return alert
+        
+    }
+    
+}
